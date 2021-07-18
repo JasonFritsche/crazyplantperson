d20eaac69dc816feb822c6b9a34807aa8a865184
jasonfritsche <fritsche.jason@gmail.com>
2021-07-18 16:49:02 -0500

    pwa setup

--- vue.config.js
+++ vue.config.js
@@ -9,4 +9,26 @@ module.exports = {
       },
     },
   },
+  pwa: {
+    name: "CrazyPlantPerson",
+    themeColor: "#2E7D32",
+    msTileColor: "#A5D6A7",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+
+    // configure the workbox plugin
+    workboxPluginMode: "InjectManifest",
+    iconPaths: {
+      favicon32: "img/icons/favicon-32x32.png",
+      favicon16: "img/icons/favicon-16x16.png",
+      appleTouchIcon: "img/icons/apple-touch-icon-152x152.png",
+      maskIcon: null,
+      msTileImage: "img/icons/msapplication-icon-144x144.png",
+    },
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: "dev/sw.js",
+      // ...other Workbox options...
+    },
+  },
 };
