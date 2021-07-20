6a17538e1cc2372337e983cb92b19761066d62d2
jasonfritsche <fritsche.jason@gmail.com>
2021-07-19 20:33:20 -0500

    added pwa support

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
-import "./registerServiceWorker";
 import router from "./router";
 import store from "./store/store";
 import vuetify from "./plugins/vuetify";
 import VCalendar from "./plugins/VCalendar";
 import VueAnimateOnScroll from "./plugins/AnimateOnScroll";
 import "@/plugins/firebase";
+import wb from "./registerServiceWorker";
+
+Vue.prototype.$workbox = wb;
 
 Vue.config.productionTip = false;
 
